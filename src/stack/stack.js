36354bb0c4429035bfe1ec4cd933a483b724e530
vaibhavmande <vaibhavrmande@gmail.com>
2019-04-17 17:24:05 +0530

    Adding push, peek and isEmpty and related tests

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -2,4 +2,16 @@ export default class Stack {
   constructor() {
     this.arrayItems = []
   }
+
+  push(item) {
+    this.arrayItems.unshift(item)
+  }
+
+  peek() {
+    return this.arrayItems[0]
+  }
+
+  isEmpty() {
+    return !this.arrayItems.length
+  }
 }
--- src/stack/stack.test.js
+++ src/stack/stack.test.js
@@ -5,4 +5,26 @@ describe('Stack', () => {
     let stack = new Stack()
     expect(stack).not.toBeNull();
   })
+
+  it('should push item', () => {
+    let stack = new Stack()
+    stack.push(1)
+    expect(stack.peek()).toBe(1)
+    expect(stack.isEmpty()).toBe(false)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.peek()).toBe(3)
+  })
+
+  it('should pop item', () => {
+
+  })
+  
+  it('should peek data from stack', () => {
+
+  })
+
+  it('should check if stack empty', () => {
+
+  })
 })
